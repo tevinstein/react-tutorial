dd7469f11fa269b37070bf34ec1c1c80b27bc8d9
tevinstein <tevinsa@yahoo.com>
2016-10-31 17:36:00 +0700

    owner ownee relationship

--- App.js
+++ App.js
@@ -3,24 +3,31 @@ import React from 'react'
 class App extends React.Component{
 	constructor(){
 		super()
-		this.state = {
-			txt: 'this is the state text',
-			cat: 0
-		}
+		this.state = { txt: 'this is state text' }
+		this.update = this.update.bind(this)
 	}
 	update(e){
-		this.setState(
-			{ txt : e.target.value }
-		)
+		this.setState({ txt : e.target.value })
 	}
 	render(){
 		return(
 			<div>
-			<input type="text" onChange={this.update.bind(this)}/>
-			<h1>{this.state.txt}</h1>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		)
 	}
+}
+
+const Widget = (props) => {
+	return(
+			<div>
+			<input type="text" onChange={props.update}/>
+			<h1>{props.txt}</h1>
+			</div>
+		)
 }
 
 export default App
